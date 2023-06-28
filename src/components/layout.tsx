1b5b1d1ca78fcdfe248d00c3242cc9d7c804926a
Alicja Dabrowska <alicja.dab27@gmail.com>
2023-06-28 21:33:29 +0200

    fix PageLayout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
       <div className="h-full w-full border-x border-slate-400 
